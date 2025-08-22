135cd9b28b87a3d19841973554464a078682279c
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2025-08-22 09:22:21 +0500

    Add models

---
+++ Database/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+public class Customer
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    [MaxLength(100)]
+    public string Name { get; set; }
+
+    [MaxLength(20)]
+    public string Phone { get; set; }
+}
---
+++ Database/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+public class Order
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    [Indexed]
+    public int CustomerId { get; set; }
+
+    public string Description { get; set; } // например "1 штук", "1 рез полный"
+    public DateTime PickupDateTime { get; set; } // дата + время
+
+    public bool IsPaid { get; set; } = false; // отметка оплаты
+}
