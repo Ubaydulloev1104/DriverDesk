5decaf6f90dcae50bdcdeadd9a03d47fff5bd297
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2025-08-25 16:40:37 +0500

    fix

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,15 +4,12 @@ namespace DriverDesk;
 
 public partial class MainPage : ContentPage
 {
-    int count = 0;
+    
 
     public MainPage()
     {
         InitializeComponent();
     }
 
-    private async void OnCounterClicked(object? sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new CustomerPage());
-    }
+
 }
