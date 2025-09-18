d9b7397210a8564be467c3c94f330eae78f8885f
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2025-09-18 08:11:03 +0500

    12

--- Class1.cs
+++ Class1.cs
@@ -13,5 +13,6 @@ namespace DriverDesk
         int c;
         public int A { get { return a; } set { a = value; } }
         public int B { get { return b; } set { b = value; } }
+        public int C { get { return c; } set { c = value; } }
     }
 }
